0d04eda9469587f84738677de2df306db0b17999
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-07-02 10:47:08 -0400

    build sisimple to do list for students

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    //form creation
+    $("<form>").appendTo('body')
+    let toDos = $("<input type=text>").appendTo('form')
+    $("<input type=submit>").appendTo('form')
+    let listSpace = $("<ol></ol>").appendTo('body')
+
+
+    //input value into input type type
+
+    //when i click the submit button a the value appends to the ul
+    $('form').submit(function () {
+        event.preventDefault()
+        let addItem = toDos.val();
+        $("<li>" + addItem + "</li>").appendTo(listSpace)
+        toDos.val(" ");
+
+
+    })
+    //when I click on a list item a line goes through it
+    $('ol').on('click', 'li', function () {
+        $(this).css('text-decoration', 'line-through')
+    })
+
+
+
+})
